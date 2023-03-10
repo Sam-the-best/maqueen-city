c54d0fac84590909ca3a8b4b889972713f5df386
Sam-the-best <126861472+Sam-the-best@users.noreply.github.com>
2023-03-10 08:25:33 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+IR.IR_callbackUser(function (message) {
+    if (message == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    } else if (message == 9) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+    } else if (message == 4) {
+        maqueen.motorStop(maqueen.Motors.M1)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    } else if (message == 6) {
+        maqueen.motorStop(maqueen.Motors.M1)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    } else if (message == 5) {
+        maqueen.motorStop(maqueen.Motors.All)
+    } else {
+    	
+    }
+})
 basic.forever(function () {
 	
 })
